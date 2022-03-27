83e3bcf55d15dacf62b1ce19280c574418eff21d
Lucas Antonio <lucaslukpe20@gmail.com>
2022-03-27 20:16:49 -0300

    Projeto criado

---
+++ src/entities/Type.java
@@ -0,0 +1,25 @@
+package entities;
+
+public enum Type {
+
+	RESERVED_WORD("Palavra Reservada"),
+	ARITHMETIC_OPERATOR("Operador Aritimetico"),
+	ASSIGNMENT_OPERATOR("Operador de Atribuição"),
+	RELATIONAL_OPERATOR("Operador Relacional"),
+	SPECIAL_CHARACTER("Caracter Especial"),
+	CHAR("Char"),
+	FLOAT("Float"),
+	END_CODE("Fim do código"),
+	IDENTIFIER("Identificador");
+
+	private String type;
+	
+	Type(String type) {
+		this.type = type;
+	}
+
+	public String getType() {
+		return type;
+	}
+	
+}
