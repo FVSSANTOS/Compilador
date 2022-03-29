96b80e9eac03ecaeb80b98ef8dc2800cc164ed2e
Lucas Antonio <lucaslukpe20@gmail.com>
2022-03-29 17:27:44 -0300

    Fix

--- src/application/Analyzer.java
+++ src/application/Analyzer.java
@@ -54,20 +54,23 @@ public class Analyzer {
                     state = 1;
 				}else if (this.isDigit(c)) {
 					str.append(c);
-                    state = 10;
+                    state = 3;
+				}else if (c == '\'') {
+					str.append(c);
+                    state = 6;
 				}else if(c == '(' || c == ')' || c == '{' || c == '}'
 						|| c == ',' || c == ';') {
 					str.append(c);
-					state = 5;
+					state = 9;
 				}else if(c == '+' || c == '-' || c == '*' || c == '/') {
 					str.append(c);
-					state = 6;
+					state = 10;
 				}else if(c == '>' || c == '<' || c == '!') {
 					str.append(c);
-					state = 7;
+					state = 11;
 				}else if(c == '=') {
 					str.append(c);
-					state = 9;
+					state = 13;
 				}else if(c == '$') {
 					state = 777;
 				}
@@ -75,107 +78,42 @@ public class Analyzer {
 			case 1:
 				if (this.isDigit(c) || this.isLetter(c) || c == '_') {
 					str.append(c);
-                    state = 4;
-				}else if (c == '.') {
-					str.append(c);
                     state = 2;
 				}else {
-					this.backChar();
-                    tokens.add(new Token(str.toString(), Type.CHAR));
-                    str = new StringBuffer();
-                    state = 0;
+					throw new RuntimeException("Error: invalid term \"" + str.toString() + "\"");
 				}
 				break;
 			case 2:
-				if (this.isDigit(c)) {
+				if (this.isDigit(c) || this.isLetter(c) || c == '_') {
 					str.append(c);
-					state = 3;
+                    state = 2;
 				}else {
-					throw new RuntimeException("Error: invalid term \"" + str.toString() + "\"");
+					if (str.toString().compareTo("main") == 0 ||
+							str.toString().compareTo("if") == 0 ||
+							str.toString().compareTo("else") == 0 ||
+							str.toString().compareTo("while") == 0 ||
+							str.toString().compareTo("do") == 0 ||
+							str.toString().compareTo("for") == 0 ||
+							str.toString().compareTo("int") == 0 ||
+							str.toString().compareTo("float") == 0 ||
+							str.toString().compareTo("char") == 0) {
+							this.backChar();
+		                    tokens.add(new Token(str.toString(), Type.RESERVED_WORD));
+						}else {
+							this.backChar();
+		                    tokens.add(new Token(str.toString(), Type.IDENTIFIER));
+						}
+						str = new StringBuffer();
+	                    state = 0;
 				}
 				break;
 			case 3:
 				if (this.isDigit(c)) {
 					str.append(c);
 					state = 3;
-				}else {
-					this.backChar();
-                    tokens.add(new Token(str.toString(), Type.FLOAT));
-                    str = new StringBuffer();
-                    state = 0;
-				}
-				break;
-			case 4:
-				if (this.isDigit(c) || this.isLetter(c) || c == '_') {
+				}else if (c == '.') {
 					str.append(c);
-                    state = 4;
-				}else {
-					if (str.toString().compareTo("main") == 0 ||
-						str.toString().compareTo("if") == 0 ||
-						str.toString().compareTo("else") == 0 ||
-						str.toString().compareTo("while") == 0 ||
-						str.toString().compareTo("do") == 0 ||
-						str.toString().compareTo("for") == 0 ||
-						str.toString().compareTo("int") == 0 ||
-						str.toString().compareTo("float") == 0 ||
-						str.toString().compareTo("char") == 0) {
-						this.backChar();
-	                    tokens.add(new Token(str.toString(), Type.RESERVED_WORD));
-					}else {
-						this.backChar();
-	                    tokens.add(new Token(str.toString(), Type.IDENTIFIER));
-					}
-					str = new StringBuffer();
-                    state = 0;
-				}
-				break;
-			case 5:
-				this.backChar();
-                tokens.add(new Token(str.toString(), Type.SPECIAL_CHARACTER));
-                str = new StringBuffer();
-                state = 0;
-				break;
-			case 6:
-				this.backChar();
-                tokens.add(new Token(str.toString(), Type.ARITHMETIC_OPERATOR));
-                str = new StringBuffer();
-                state = 0;
-				break;
-			case 7:
-				if (c == '=') {
-					str.append(c);
-					state = 8;
-				}else {
-					this.backChar();
-                    tokens.add(new Token(str.toString(), Type.RELATIONAL_OPERATOR));
-                    str = new StringBuffer();
-                    state = 0;
-				}
-				break;
-			case 8:
-				this.backChar();
-                tokens.add(new Token(str.toString(), Type.RELATIONAL_OPERATOR));
-                str = new StringBuffer();
-                state = 0;
-                break;
-			case 9:
-				if (c == '=') {
-					str.append(c);
-					state = 8;
-				}else {
-					this.backChar();
-                    tokens.add(new Token(str.toString(), Type.ASSIGNMENT_OPERATOR));
-                    str = new StringBuffer();
-                    state = 0;
-				}
-				break;
-			case 10:
-				if (this.isDigit(c)) {
-					str.append(c);
-					state = 10;
-				}else if(c == '.') {
-					str.append(c);
-					state = 2;
+					state = 4;
 				}else {
 					this.backChar();
                     tokens.add(new Token(str.toString(), Type.INT));
@@ -183,6 +121,90 @@ public class Analyzer {
                     state = 0;
 				}
 				break;
+			case 4:
+				if (this.isDigit(c)) {
+					str.append(c);
+                    state = 5;
+				}else {
+					throw new RuntimeException("Error: invalid term \"" + str.toString() + "\"");
+				}
+				break;
+			case 5:
+				if (this.isDigit(c)) {
+					str.append(c);
+                    state = 5;
+				}else {
+					this.backChar();
+	                tokens.add(new Token(str.toString(), Type.SPECIAL_FLOAT));
+	                str = new StringBuffer();
+	                state = 0;
+				}
+				break;
+			case 6:
+				if (this.isDigit(c) || this.isLetter(c)) {
+					str.append(c);
+                    state = 7;
+				}else {
+					throw new RuntimeException("Error: invalid term \"" + str.toString() + "\"");
+				}
+				break;
+			case 7:
+				if (c == '\'') {
+					str.append(c);
+					state = 8;
+				}else {
+					throw new RuntimeException("Error: invalid term \"" + str.toString() + "\"");
+				}
+				break;
+			case 8:
+				this.backChar();
+                tokens.add(new Token(str.toString(), Type.CHAR));
+                str = new StringBuffer();
+                state = 0;
+                break;
+			case 9:
+					this.backChar();
+                    tokens.add(new Token(str.toString(), Type.SPECIAL_CHARACTER));
+                    str = new StringBuffer();
+                    state = 0;
+				break;
+			case 10:
+					this.backChar();
+                    tokens.add(new Token(str.toString(), Type.ARITHMETIC_OPERATOR));
+                    str = new StringBuffer();
+                    state = 0;
+				break;
+			case 11:
+				if (c == '=') {
+					str.append(c);
+					state = 12;
+				}else {
+					if (str.toString().compareTo("!") == 0) {
+						throw new RuntimeException("Error: invalid term \"" + str.toString() + "\"");
+					}else {
+						this.backChar();
+	                    tokens.add(new Token(str.toString(), Type.RELATIONAL_OPERATOR));
+	                    str = new StringBuffer();
+	                    state = 0;
+					}
+				}
+				break;
+			case 12:
+				this.backChar();
+                tokens.add(new Token(str.toString(), Type.RELATIONAL_OPERATOR));
+                str = new StringBuffer();
+                state = 0;
+				break;
+			case 13:
+				if (c == '=') {
+					str.append(c);
+					state = 12;
+				}else {
+					this.backChar();
+	                tokens.add(new Token(str.toString(), Type.ASSIGNMENT_OPERATOR));
+	                str = new StringBuffer();
+	                state = 0;
+				}
 			case 777:
 				tokens.add(new Token(str.toString(), Type.END_CODE)); 
 				break;
